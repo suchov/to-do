cba841b95625a36a1709edb2c04ea769bc6ffec0
Artem Sychov <[artem.suchov@gmail.com]>
2023-09-06 17:16:34 +0200

    just don't complify the output no decoupling for now

--- bonus12.py
+++ bonus12.py
@@ -6,7 +6,12 @@ def convert(feet_inches):
     feet = float(feet_inches[0])
     inches = float(feet_inches[1])
     meters = feet * 0.3048 + inches * 0.0254
-    return f"{feet} feet and {inches} inches is equal to {meters} meters."
+    return meters
 
 
-print(convert(feet_inches))
+result = convert(feet_inches)
+
+if result < 1:
+    print("Kid is too small")
+else:
+    print("Kid can use the slide")
