b2d0be67087a900693590f8fb7c9170293c99665
Artem Sychov <[artem.suchov@gmail.com]>
2023-09-07 13:38:24 +0200

    functions are now decoupled so they do one thing but good

--- bonus12.py
+++ bonus12.py
@@ -2,14 +2,21 @@
 feet_inches = input("Enter feet and inches: ").split(" ")
 
 
-def convert(feet_inches):
+def parse(feet_inches):
     feet = float(feet_inches[0])
     inches = float(feet_inches[1])
+    return {"feet": feet, "inches": inches}
+
+
+def convert(feet, inches):
     meters = feet * 0.3048 + inches * 0.0254
     return meters
 
 
-result = convert(feet_inches)
+parsed = parse(feet_inches)
+result = convert(parsed['feet'], parsed['inches'])
+
+print(f"{parsed['feet']} feet and {parsed['inches']} is equal to {result}")
 
 if result < 1:
     print("Kid is too small")
