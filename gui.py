468fe8afb02888501c862e7763c8526930913a3a
Artem Sychov <[artem.suchov@gmail.com]>
2023-09-20 19:37:23 +0200

    add the add to-do functionality to my GUI.py

--- gui.py
+++ gui.py
@@ -3,10 +3,22 @@ import PySimpleGUI as sg
 
 
 lable = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window('My To-Do App', layout=[[lable], [input_box, add_button]])
+window = sg.Window('My To-Do App',
+                   layout=[[lable], [input_box, add_button]],
+                   font=('Helvetica', 20))
 
-window.read()
+while True:
+    event, values = window.read()
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
 window.close()
