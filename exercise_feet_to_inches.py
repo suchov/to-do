791389580e64efe309093cbd8b1f48f28bdac01b
Artem Sychov <[artem.suchov@gmail.com]>
2023-10-31 15:22:30 +0100

    finish exercise feet to inches

--- exercise_feet_to_inches.py
+++ exercise_feet_to_inches.py
@@ -1,3 +1,25 @@
 import PySimpleGUI as sg
 
-label1 = sg.Text("Feet to inches conventor")
+label1 = sg.Text("Enter feet:")
+input1 = sg.Input(key="feets")
+
+
+label2 = sg.Text("Enter inches:")
+input2 = sg.Input(key="inches")
+
+convert_button = sg.Button("Convert")
+output_lable = sg.Text(key="output")
+
+
+window = sg.Window("Conventor",
+                   layout=[[label1, input1], [label2, input2],
+                           [convert_button, output_lable]])
+
+while True:
+    event, values = window.read()
+    feets = float(values["feets"])
+    inches = float(values["inches"])
+
+    window["output"].update(value=feets * 0.3048 + inches * 0.0254)
+
+window.close()
