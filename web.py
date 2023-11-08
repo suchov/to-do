fb741cc0cd1616d49c15ce2a9945fcdbb0500a4c
Artem Sychov <[artem.suchov@gmail.com]>
2023-11-08 11:55:51 +0100

    add the ability to add todos

--- web.py
+++ web.py
@@ -6,6 +6,13 @@ import functions
 
 todos = functions.get_todos()
 
+
+def add_todo():
+    todo = st.session_state['new_todo'] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title("My TODO App")
 st.subheader("This is my todo app")
 st.write("This app is to increase your productivity")
@@ -13,4 +20,8 @@ st.write("This app is to increase your productivity")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add new todo...")
+st.text_input(label=" ", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
+
+# show the session_state at the bottom
+st.session_state
