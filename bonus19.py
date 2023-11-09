1a8383541d358bbadf39097426f7619f2fdf35f1
Artem Sychov <[artem.suchov@gmail.com]>
2023-11-09 10:40:04 +0100

    upload image exercise

--- bonus19.py
+++ bonus19.py
@@ -2,6 +2,8 @@ import streamlit as st
 from PIL import Image
 
 
+uploaded_image = st.file_uploader("Upload Image")
+
 with st.expander("Start camera"):
     camera_image = st.camera_input("Camera")
 
@@ -14,4 +16,11 @@ if camera_image:
     gray_img = img.convert("L")
 
     # Render gray scale image on the web page
+    st.image(gray_img)
+
+if uploaded_image:
+    img = Image.open(uploaded_image)
+
+    gray_img = img.convert("L")
+
     st.image(gray_img)
