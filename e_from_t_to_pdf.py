5af7331a7d9ab592dde5d34129f979c9f68650e4
Artem Sychov <artem@sychov.dev>
2024-03-04 09:24:09 +0100

    add some coments to review

--- e_from_t_to_pdf.py
+++ e_from_t_to_pdf.py
@@ -10,10 +10,14 @@ filepaths = glob.glob("Text+Files/*.txt")
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 for filepath in filepaths:
+    # read the file - we probably don't need it for the first iteration
     df = pd.read_csv(filepath)
+    # adding page
     pdf.add_page()
+    # get a file path ...
     filename = Path(filepath).stem
     name = filename.capitalize()
+    # style for the pdf page
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=name)
 
